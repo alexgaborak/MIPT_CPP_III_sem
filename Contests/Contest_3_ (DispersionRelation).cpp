18700f19b7f9cdcdbda4d1887580e6e036df8b76
alexgaborak <alexgaborak@phystech.edu>
2023-09-26 14:21:39 +0300

    Contest_3_ (DispersionRelation).cpp

---
+++ Contests/Contest_3_ (DispersionRelation).cpp
@@ -0,0 +1,42 @@
+//
+// Created by alexg on 9/16/2023.
+//
+class DispersionRelationA : public DispersionRelation
+{
+public:
+    double const k;
+
+    DispersionRelationA(double k): k(k) { }
+
+    double operator()(double omega) const override
+    {
+        return implicit_dispersion_relation_A(omega, k);
+    }
+};
+
+class DispersionRelationB : public DispersionRelation
+{
+public:
+    double const k, alpha;
+
+    DispersionRelationB(double k, double alpha): k(k), alpha(alpha) { }
+
+    double operator()(double omega) const override
+    {
+        return implicit_dispersion_relation_B(omega, k, alpha);
+    }
+};
+
+DispersionRelation const * construct_dispersion_relation_A(double k)
+{
+    return new DispersionRelationA(k);
+}
+
+DispersionRelation const * construct_dispersion_relation_B(double k, double alpha)
+{
+    return new DispersionRelationB(k, alpha);
+}
+
+    call(b);
+
+}
