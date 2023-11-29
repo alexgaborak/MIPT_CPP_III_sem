c8027f40488f3f7719d871e33da3cdb6dbfc98b5
alexgaborak <alexgaborak@phystech.edu>
2023-11-29 15:26:04 +0300

    Lab 3 test asymptotic

---
+++ Lab 3 (Rating) 18-11-2023 (SkipList)/skiplist.h
@@ -0,0 +1,201 @@
+//
+// Created by alexg on 11/29/2023.
+//
+
+#ifndef MIPT_CPP_III_SEM_SKIPLIST_H
+#define MIPT_CPP_III_SEM_SKIPLIST_H
+
+#endif //MIPT_CPP_III_SEM_SKIPLIST_H
+
+#include <vector>
+#include <iterator>
+#include <random>
+#include <algorithm>
+
+template <typename T, typename Compare = std::less<T>>
+class SkipList {
+private:
+    struct Node {
+        T value;
+        std::vector<Node*> forward;
+
+        Node(const T& val, size_t level) : value(val), forward(level + 1, nullptr) {}
+
+        friend class Iterator;
+    };
+
+    static constexpr double PROBABILITY = 0.5;
+    size_t maxLevel;
+    size_t size;
+    Node* header;
+    Compare comparator;
+
+    size_t randomLevel() {
+        size_t level = 0;
+        std::default_random_engine generator;
+        std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+        while (distribution(generator) < PROBABILITY && level < maxLevel) {
+            level++;
+        }
+
+        return level;
+    }
+
+public:
+    class Iterator {
+    public:
+        explicit Iterator(Node* node) : current(node) {}
+
+        T& operator*() const {
+            return current->value;
+        }
+
+        Iterator& operator++() {
+            current = current->forward[0];
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            Iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        friend bool operator==(const Iterator& lhs, const Iterator& rhs) {
+            return lhs.current == rhs.current;
+        }
+
+        friend bool operator!=(const Iterator& lhs, const Iterator& rhs) {
+            return !(lhs == rhs);
+        }
+
+        Node* getCurrent() const {
+            return current;
+        }
+
+    private:
+        Node* current;
+    };
+
+    // Constructors, destructors, and other methods as needed
+
+    SkipList() : maxLevel(0), size(0), header(new Node(T(), 0)) {}
+
+    explicit SkipList(const Compare& comp) : maxLevel(0), size(0), header(new Node(T(), 0)), comparator(comp) {}
+
+    ~SkipList() {
+        clear();
+        delete header;
+    }
+
+    bool empty() const {
+        return size == 0;
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+
+    void insert(const T& value) {
+        std::vector<Node*> update(header->forward.size(), nullptr);
+        Node* current = header;
+
+        for (int i = static_cast<int>(header->forward.size()) - 1; i >= 0; --i) {
+            while (current->forward[i] != nullptr && comparator(current->forward[i]->value, value)) {
+                current = current->forward[i];
+            }
+            update[i] = current;
+        }
+
+        size_t newLevel = randomLevel();
+
+        if (newLevel > maxLevel) {
+            update.resize(newLevel + 1, header);
+            maxLevel = newLevel;
+        }
+
+        Node* newNode = new Node(value, newLevel);
+
+        for (size_t i = 0; i <= newLevel; ++i) {
+            newNode->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = newNode;
+        }
+
+        size++;
+    }
+
+    Iterator find(const T& key) {
+        Node* current = header;
+
+        for (int i = static_cast<int>(header->forward.size()) - 1; i >= 0; --i) {
+            while (current->forward[i] != nullptr && comparator(current->forward[i]->value, key)) {
+                current = current->forward[i];
+            }
+        }
+
+        current = current->forward[0];
+
+        if (current != nullptr && !comparator(key, current->value)) {
+            return Iterator(current);
+        } else {
+            return end();
+        }
+    }
+
+    void erase(const Iterator& it) {
+        Node* current = header;
+        std::vector<Node*> update(header->forward.size(), nullptr);
+
+        for (int i = static_cast<int>(header->forward.size()) - 1; i >= 0; --i) {
+            while (current->forward[i] != nullptr && current->forward[i] != it.getCurrent()) {
+                current = current->forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current->forward[0];
+
+        if (current == it.getCurrent()) {
+            for (size_t i = 0; i < current->forward.size(); ++i) {
+                update[i]->forward[i] = current->forward[i];
+            }
+
+            delete current;
+
+            // Update maxLevel if necessary
+            while (maxLevel > 0 && header->forward[maxLevel] == nullptr) {
+                maxLevel--;
+            }
+
+            size--;
+        }
+    }
+
+    Iterator begin() const {
+        return Iterator(header->forward[0]);
+    }
+
+    Iterator end() const {
+        return Iterator(nullptr);
+    }
+
+    // Other methods and operators...
+
+    // Helper functions...
+
+    void clear() {
+        Node* current = header->forward[0];
+        while (current != nullptr) {
+            Node* next = current->forward[0];
+            delete current;
+            current = next;
+        }
+
+        header->forward.clear();
+        maxLevel = 0;
+        size = 0;
+    }
+
+    // Other helper functions...
+};
---
+++ Lab 3 (Rating) 18-11-2023 (SkipList)/skiplist_ins.cpp
@@ -0,0 +1,52 @@
+//
+// Created by alexg on 11/29/2023.
+//
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include "SkipList.h"  // Include the SkipList class definition
+
+int main() {
+    // Open the output file
+    std::ofstream outputFile("search_p_0.5.txt");
+
+    // Seed for random number generation
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Range of elements to test
+    int start = 10;
+    int end = 20000;
+    int increment = 10;
+
+    for (int numElements = start; numElements <= end; numElements += increment) {
+        // Create a SkipList
+        SkipList<int> skipList;
+
+        // Measure the time taken to insert elements
+        auto startTime = std::chrono::high_resolution_clock::now();
+
+        // Fill the list with random numbers
+        std::uniform_int_distribution<> dis(1, numElements * 10);
+        for (int i = 0; i < numElements; ++i) {
+            skipList.insert(dis(gen));
+        }
+
+        auto endTime = std::chrono::high_resolution_clock::now();
+
+        // Calculate the insertion time in nanoseconds
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+
+        // Record the results in the output file
+        outputFile << numElements << " " << duration << std::endl;
+
+        // Output progress to console
+        std::cout << "Processed: " << numElements << " elements" << std::endl;
+    }
+
+    // Close the output file
+    outputFile.close();
+
+    return 0;
+}
