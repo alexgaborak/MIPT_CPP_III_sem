ebda04cfd7fc0d26aa4d3c7192bb54f91c3e34e6
alexgaborak <alexgaborak@phystech.edu>
2023-12-09 01:11:23 +0300

    probability.cpp without tests

--- Lab 4 (Rating) 25-11-2023 (Probability)/probability.cpp
+++ Lab 4 (Rating) 25-11-2023 (Probability)/probability.cpp
@@ -48,6 +48,71 @@ public:
     }
 };
 
+
+class ContinuousStatesWithOmissions : public State {
+private:
+    int const beg, end, omission;
+
+public:
+    ContinuousStatesWithOmissions(int beg, int end, int omission) : beg(beg), end(end), omission(omission) {}
+
+    bool contains(int s) const override {
+        return s >= beg && s <= end && s != omission;
+    }
+};
+
+class ContinuousStatesWithAdditions : public State {
+private:
+    int const beg, end, addition;
+
+public:
+    ContinuousStatesWithAdditions(int beg, int end, int addition) : beg(beg), end(end), addition(addition) {}
+
+    bool contains(int s) const override {
+        return (s >= beg && s <= end) || s == addition;
+    }
+};
+
+class ContinuousStatesWithOmissionsAndAdditions : public State {
+private:
+    int const beg, end, omission, addition;
+
+public:
+    ContinuousStatesWithOmissionsAndAdditions(int beg, int end, int omission, int addition)
+            : beg(beg), end(end), omission(omission), addition(addition) {}
+
+    bool contains(int s) const override {
+        return (s >= beg && s <= end) && s != omission || s == addition;
+    }
+};
+
+class UnionState : public State {
+private:
+    const State &state1;
+    const State &state2;
+
+public:
+    UnionState(const State &state1, const State &state2) : state1(state1), state2(state2) {}
+
+    bool contains(int s) const override {
+        return state1.contains(s) || state2.contains(s);
+    }
+};
+
+class IntersectionState : public State {
+private:
+    const State &state1;
+    const State &state2;
+
+public:
+    IntersectionState(const State &state1, const State &state2) : state1(state1), state2(state2) {}
+
+    bool contains(int s) const override {
+        return state1.contains(s) && state2.contains(s);
+    }
+};
+
+
 class ProbabilityTest {
 private:
     unsigned seed;
@@ -74,9 +139,23 @@ int main(int argc, const char *argv[]) {
     DiscreteState d(1);
     SegmentState s(0, 10);
     SetState ss({1, 3, 5, 7, 23, 48, 57, 60, 90, 99});
+
+    ContinuousStatesWithOmissions co(0, 10, 5);
+    ContinuousStatesWithAdditions ca(0, 10, 15);
+    ContinuousStatesWithOmissionsAndAdditions coa(0, 10, 5, 15);
+    UnionState unionState(ss, ca);
+    IntersectionState intersectionState(s, co);
+
     ProbabilityTest pt(10, 0, 100, 100000);
-    std::cout << pt(d) << std::endl;
-    std::cout << pt(s) << std::endl;
-    std::cout << pt(ss) << std::endl;
+
+    std::cout << "DiscreteState: " << pt(d) << std::endl;
+    std::cout << "SegmentState: " << pt(s) << std::endl;
+    std::cout << "SetState: " << pt(ss) << std::endl;
+    std::cout << "ContinuousStatesWithOmissions: " << pt(co) << std::endl;
+    std::cout << "ContinuousStatesWithAdditions: " << pt(ca) << std::endl;
+    std::cout << "ContinuousStatesWithOmissionsAndAdditions: " << pt(coa) << std::endl;
+    std::cout << "UnionState: " << pt(unionState) << std::endl;
+    std::cout << "IntersectionState: " << pt(intersectionState) << std::endl;
+
     return 0;
 }
